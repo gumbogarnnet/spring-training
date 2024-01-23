412c7827420eb3b6a3d5eaab16c6ccc0f9dc0dcf
gumbogarnnet <42249414+gumbogarnnet@users.noreply.github.com>
2024-01-23 09:43:15 +0000

    Update author.java

--- src/main/java/com/example/Garnspring/model/author.java
+++ src/main/java/com/example/Garnspring/model/author.java
@@ -35,16 +35,16 @@ public class author {
         
     }
      public long getid(){return id;}
-     public void setid (string id){this id=id}
+     public void setid (string id){this id=id;}
      
      public String getfrstname(){return frstname;}
-     public void setfrstname(String frstname){this frstname=frstname}
+     public void setfrstname(String frstname){this frstname=frstname;}
      
       public String getlstname(){return lstname;}
-     public void setlstname(String lstname){this lstname=lstname}
+     public void setlstname(String lstname){this lstname=lstname;}
      
       public Set<Book> getBooks(){return books;}
-     public void setBooks  (Set<Books> books){this books=books}
+     public void setBooks  (Set<Books> books){this books=books;}
 }
     
     
